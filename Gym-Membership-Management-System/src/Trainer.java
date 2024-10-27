0a71c80441a87496cdbb42ec4f3254f881d60708
marwan-ahmad07 <154096704+marwan-ahmad07@users.noreply.github.com>
2024-10-28 01:29:53 +0300

    Trainer class and trainer database class still nc

---
+++ Gym-Membership-Management-System/src/Trainer.java
@@ -0,0 +1,28 @@
+public class Trainer {
+    private String trainerId;
+    private String name;
+    private String email;
+    private String specialty;
+    private String phoneNumber;
+
+    public Trainer(String trainerId, String name, String email, String specialty, String phoneNumber) {
+        this.trainerId = trainerId;
+        this.name = name;
+        this.email = email;
+        this.specialty = specialty;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String lineRepresentation (){
+        return String.join(", ", trainerId, name, email, specialty, phoneNumber);
+    }
+
+    public String getSearchKey(){
+        return trainerId;
+    }
+
+
+
+
+
+}
---
+++ Gym-Membership-Management-System/src/TrainerDatabase.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainerDatabase {
+    private String fileName;
+    List<Trainer> records;
+
+    public TrainerDatabase(String fileName) {
+        this.fileName = fileName;
+        records = new ArrayList<>();
+    }
+
+    public Trainer createRecordFrom(String line) {
+        String[] data = line.split(",");
+        String trainerId = data[0];
+        String name = data[1];
+        String email = data[2];
+        String specialty = data[3];
+        String phoneNumber = data[4];
+        return new Trainer(trainerId,name,email,specialty,phoneNumber);
+    }
+    public void readFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("Trainers.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Trainer trainer = createRecordFrom(line);
+                records.add(trainer);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from file: " + e.getMessage());
+        }
+    }
+    public List<Trainer> returnAllRecords() {
+        return records;
+    }
+
+}
