2569adae3694a8b8dbf075a073ac298e905de7d7
Mohamed Khamis <108624080+MohamedHKhamis@users.noreply.github.com>
2024-11-04 18:29:33 +0200

    tranierole anc addmember window

---
+++ Gym-Membership-Management-System/src/AddMember.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class AddMember extends JFrame {
+    private JPanel Entry;
+    private JTextField IdField;
+    private JTextField nameField;
+    private JTextField membershipField;
+    private JTextField emailField;
+    private JTextField phoneNumberField;
+    private JButton addButton;
+    private JTextField statusFeild;
+
+    public AddMember() {
+        setVisible(true);
+        setSize(new Dimension(600, 700));
+        setContentPane(Entry);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        new AddMember();
+    }
+}
---
+++ Gym-Membership-Management-System/src/FileNames.java
@@ -0,0 +1,7 @@
+public interface FileNames {
+    String TRAINER_FILENAME = "Trainers.txt";
+    String MEMBER_FILENAME = "Members.txt";
+    String CLASS_FILENAME = "Class.txt";
+    String REGISTRATION_FILENAME = "Registration.txt";
+
+}
